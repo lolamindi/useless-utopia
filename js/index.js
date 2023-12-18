1744eba193d34ff16312786720501558bf213a28
lolamindi <laura_mday@yahoo.co.uk>
2023-12-18 14:34:57 +0100

    change home button and add delete all button

--- js/index.js
+++ js/index.js
@@ -1,46 +1,83 @@
-import { get_fact } from './data.js';
-import { add_favorite, list_favorites } from './favorites.js';
+import { get_fact } from "./data.js";
+import { add_favorite, list_favorites } from "./favorites.js";
 
-const fact_text = document.querySelector(".card_facts_text")
+const fact_text = document.querySelector(".card_facts_text");
 
 const new_fact = async () => {
-    const fact = await get_fact();
-    fact_text.innerHTML = fact;
-    return fact;
+  const fact = await get_fact();
+  fact_text.innerHTML = fact;
+  return fact;
 };
 
 const favorite = () => {
-    add_favorite(fact_text.innerHTML);
+  add_favorite(fact_text.innerHTML);
 };
 
 const favorites = () => {
     const favorites_list = list_favorites();
     const favorites_list_elem = document.getElementById("list_favorites");
-    favorites_list_elem.innerHTML = favorites_list.length === 0
-        ? "<li>No favorites yet</li>"
-        : favorites_list.map((fact) => {
-            const list_item = document.createElement("li");
-            list_item.textContent = fact;
-            return list_item.outerHTML;
-        }).join('');
+  
+    if (localStorage.getItem("favorites") === null || favorites_list.length === 0) {
+      favorites_list_elem.innerHTML = "<li>No favorites yet</li>";
+    } else {
+      favorites_list_elem.innerHTML = favorites_list
+        .map((fact) => {
+          const list_item = document.createElement("li");
+          list_item.textContent = fact;
+          return list_item.outerHTML;
+        })
+        .join("");
+    }
+  
     document.querySelector("main").style.display = "none";
     document.getElementById("home").style.display = "block";
-};
-
-
-const refresh = () => {
+  };
+  
+  const refresh = () => {
     location.reload();
+  };
+  
+  const clear = () => {
+    localStorage.clear();
+    favorites();
+  };
+
+// const favorites = () => {
+//   const favorites_list = list_favorites();
+//   const favorites_list_elem = document.getElementById("list_favorites");
+//   favorites_list_elem.innerHTML =
+//     favorites_list.length === 0
+//       ? "<li>No favorites yet</li>"
+//       : favorites_list
+//           .map((fact) => {
+//             const list_item = document.createElement("li");
+//             list_item.textContent = fact;
+//             return list_item.outerHTML;
+//           })
+//           .join("");
+//   document.querySelector("main").style.display = "none";
+//   document.getElementById("home").style.display = "block";
+// };
+
+// const refresh = () => {
+//   location.reload();
+// };
+
+// const clear = () => {
+//   localStorage.clear();
+//   favorites();
+// };
+
+
+const home = () => {
+  refresh();
 };
 
-const clear = () => {
-    localStorage.clear();
-    refresh();
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById("button_new_fact").addEventListener("click", new_fact);
-    document.getElementById("button_add_favorite").addEventListener("click", favorite);
-    document.getElementById("button_favorites").addEventListener("click", favorites);
-    document.querySelector(".page_title").addEventListener("click", refresh);
-    document.getElementById("home").addEventListener("click", clear);
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("button_new_fact").addEventListener("click", new_fact);
+  document.getElementById("button_add_favorite").addEventListener("click", favorite);
+  document.getElementById("button_favorites").addEventListener("click", favorites);
+  document.querySelector(".page_title").addEventListener("click", refresh);
+  document.getElementById("button_home").addEventListener("click", home);
+  document.getElementById("button_delete").addEventListener("click", clear);
 });
